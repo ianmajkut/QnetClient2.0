fbba2f7e913303f507888c874df84abbd86d5b4d
Lucas Affre <lucasaffre@gmail.com>
2020-06-29 22:08:39 -0300

    cambiar recycleview

--- app/src/main/java/com/qnet/qnetclient/data/repo/FirebaseRepo.kt
+++ app/src/main/java/com/qnet/qnetclient/data/repo/FirebaseRepo.kt
@@ -3,15 +3,18 @@ package com.qnet.qnetclient.data.repo
 import android.content.ContentValues.TAG
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.functions.FirebaseFunctions
+import com.ian.bottomnavigation.ui.home.Model
 
 class FirebaseRepo {
     private val db = FirebaseFirestore.getInstance()
     private lateinit var functions: FirebaseFunctions
     private lateinit var mAuth: FirebaseAuth
+    private var aux = 0
 
     fun uploadData(name:String,dni:Int) {
         mAuth = FirebaseAuth.getInstance()
@@ -50,5 +53,27 @@ class FirebaseRepo {
                 val result = task.result?.data.toString()
                 result
             }
+    }
+    fun getLocalData(): LiveData<MutableList<Model>> {
+        val mutableData = MutableLiveData<MutableList<Model>>()
+        db.collection("locales").get().addOnSuccessListener {result ->
+            val listData = mutableListOf<Model>()
+            for (document in result){
+                val title = document.getString("title")
+                val descripcion = document.getString("descripcion")
+                val num = document.getString("title")
+                val dist = document.getString("descripcion")
+                val image = document.getString("image")
+                val local = Model(title, descripcion, num, dist, image)
+                listData.add(local)
+            }
+            mutableData.value = listData
+        }
+        if (mutableData == null&&aux<5) {
+            aux++
+            getLocalData()
+        }
+        aux=0
+        return mutableData
     }
 }
---
+++ app/src/main/java/com/qnet/qnetclient/data/repo/mainRepo.kt
@@ -0,0 +1,11 @@
+package com.qnet.qnetclient.data.repo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.ian.bottomnavigation.ui.home.Model
+
+class mainRepo {
+
+
+}
