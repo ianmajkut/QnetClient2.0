0481e726addb08a5cff77e96984f06f13c105d3a
Lucas Affre <lucasaffre@gmail.com>
2020-07-02 16:52:18 -0300

    Fragment 2 Funcionando

--- app/src/main/java/com/qnet/qnetclient/appusuario/ui/home/Model.kt
+++ app/src/main/java/com/qnet/qnetclient/appusuario/ui/home/Model.kt
@@ -1,3 +1,39 @@
 package com.ian.bottomnavigation.ui.home
 
-class Model (var title:String?,var descripcion:String?,var num:String?,var dist:String?, var image:String? )
+import android.os.Parcel
+import android.os.Parcelable
+
+
+data class Model (var title:String?,var descripcion:String?,var num:String?,var dist:String?, var image:String? ) :
+    Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(descripcion)
+        parcel.writeString(num)
+        parcel.writeString(dist)
+        parcel.writeString(image)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Model> {
+        override fun createFromParcel(parcel: Parcel): Model {
+            return Model(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Model?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
