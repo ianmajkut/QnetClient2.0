f70e81663b8aeca97e9daf7d94f18d17e3a7c1ab
TheoSt1789 <66036447+TheoSt1789@users.noreply.github.com>
2020-07-09 17:14:17 -0300

    verificacion 100% andando

--- app/src/main/java/com/qnet/qnetclient/data/AuthUser.kt
+++ app/src/main/java/com/qnet/qnetclient/data/AuthUser.kt
@@ -2,20 +2,30 @@ package com.qnet.qnetclient.data
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
+import org.jetbrains.annotations.NotNull
+import kotlin.coroutines.coroutineContext
 
 class AuthUser {
-
     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
     private var aux:Boolean = false
 
     fun createAccount(email: String, password: String) {
-
+        Log.i("Verif", "createUser() AuthUser.kt")
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    mAuth.currentUser?.sendEmailVerification()
+                    mAuth.currentUser!!.sendEmailVerification().addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.i("Verif", "E-Mail sent to ${mAuth.currentUser!!.email}")
+                        } else {
+                            Log.i("Verif", "Exception: ", task.exception)
+                        }
+                    }
                 } else {
 
                 }
@@ -31,10 +41,4 @@ class AuthUser {
             }
         return aux
     }
-
-
-
-
-
-
 }
