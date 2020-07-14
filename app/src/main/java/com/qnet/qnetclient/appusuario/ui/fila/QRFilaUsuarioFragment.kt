bad4d1ca9d1fa3ebe7662d556c12314d1668b3ec
ianmajkut <65124829+ianmajkut@users.noreply.github.com>
2020-07-14 17:37:15 -0300

    Hice el generador del codigo qr

--- app/src/main/java/com/qnet/qnetclient/appusuario/ui/fila/QRFilaUsuarioFragment.kt
+++ app/src/main/java/com/qnet/qnetclient/appusuario/ui/fila/QRFilaUsuarioFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.qnet.qnetclient.R
 
 
@@ -13,7 +18,27 @@ class QRFilaUsuarioFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_q_r_fila_usuario, container, false)
+        val layout=inflater.inflate(R.layout.fragment_q_r_fila_usuario, container, false)
+
+        val etText = layout.findViewById<EditText>(R.id.et_text)
+        val btnGen=layout.findViewById<Button>(R.id.btn_generate)
+        val ivBarcode=layout.findViewById<ImageView>(R.id.iv_barcode)
+
+        btnGen.setOnClickListener {
+            try {
+                val encoder= BarcodeEncoder()
+                val bitmap= encoder.encodeBitmap(etText.text.toString(), BarcodeFormat.QR_CODE,
+                    500, 500)
+                ivBarcode.setImageBitmap(bitmap)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+
+        }
+
+
+        return layout
+
     }
 
 }
