a1ae4b0e4d40bacdfc586277345381812d93ff62
ianmajkut <ianedwinmajkut@gmail.com>
2020-07-20 15:51:24 -0300

    Progress Bar incluido

---
+++ app/src/main/java/com/qnet/qnetclient/loginregister_usuario/CommonUtils.kt
@@ -0,0 +1,28 @@
+package com.qnet.qnetclient.loginregister_usuario
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import androidx.core.graphics.drawable.toDrawable
+import com.qnet.qnetclient.R
+
+object CommonUtils{
+
+fun showLoadingDialog(context:Context):Dialog{
+
+    val progressDialog=Dialog(context)
+
+    progressDialog.let {
+        it.show()
+        it.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+       it.setContentView(R.layout.progress_dialog)
+        it.setCancelable(false)
+        it.setCanceledOnTouchOutside(false)
+
+        return it
+    }
+
+}
+
+
+}
--- app/src/main/java/com/qnet/qnetclient/loginregister_usuario/login_register.kt
+++ app/src/main/java/com/qnet/qnetclient/loginregister_usuario/login_register.kt
@@ -1,6 +1,8 @@
 package com.qnet.qnetclient.loginregister_usuario
 
+import android.app.Dialog
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +16,7 @@ import kotlinx.android.synthetic.main.fragment_login_register.*
 
 class login_register : Fragment() {
     private lateinit var viewModel: FirestoreViewModel
+    private var loadingDialog: Dialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,8 +44,15 @@ class login_register : Fragment() {
         val password = edtxt_Password.text.toString().trim()
 
         if (name.isNotEmpty() && password.isNotEmpty()) {
+
+            //Aca progress bar
+            showLoading()
+            Handler().postDelayed({
+                hideLoading()
+
+            }, 5000)
            obsever(name, password)
-            //@Ian falta poner un progress bar para ver el progreso
+
         } else {
             Toast.makeText(activity, "Error Campos Incompletos", Toast.LENGTH_SHORT).show()
         }
@@ -53,10 +63,22 @@ class login_register : Fragment() {
             if(it) {
                 Toast.makeText(activity, "Ok", Toast.LENGTH_SHORT).show()
                 viewModel.localesCercanos()
+
+                //aca termina
                 findNavController().navigate(R.id.menu_principal_action)
             }else{
                 Toast.makeText(activity, "Usuario no Registrado", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    private fun hideLoading(){
+        loadingDialog?.let { if (it.isShowing)it.cancel() }
+    }
+
+    private fun showLoading(){
+        hideLoading()
+        loadingDialog=CommonUtils.showLoadingDialog(requireContext())
+    }
+
 }
