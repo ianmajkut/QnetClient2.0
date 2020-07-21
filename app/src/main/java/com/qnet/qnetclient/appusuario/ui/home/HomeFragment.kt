2f8ec1ab3871132b803ff52a739b08dfff18ca95
ianmajkut <ianedwinmajkut@gmail.com>
2020-07-21 16:29:18 -0300

    Boton para salir de la fila

--- app/src/main/java/com/qnet/qnetclient/appusuario/ui/home/HomeFragment.kt
+++ app/src/main/java/com/qnet/qnetclient/appusuario/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.facebook.shimmer.ShimmerFrameLayout
 import com.qnet.qnetclient.R
 import com.qnet.qnetclient.appusuario.AppUser
@@ -24,15 +25,22 @@ class HomeFragment : Fragment() {
 
         val layout=inflater.inflate(R.layout.fragment_home, container, false)
        val shimmer_view_container= layout.findViewById<ShimmerFrameLayout>(R.id.shimmer_view_container)
-
+      //  val swipeRefreshLayout=layout.findViewById<SwipeRefreshLayout>(R.id.swipeRefreshLayout)
         val recycler = layout.findViewById<RecyclerView>(R.id.recyclerview)
+
+       /*swipeRefreshLayout.setOnClickListener {
+            shimmer_view_container.startShimmer()
+            observerData()
+
+
+
+        }*/
 
         shimmer_view_container.startShimmer()
 
         observerData()
 
-       /* shimmer_view_container.stopShimmer()
-        shimmer_view_container.visibility=View.GONE*/
+
 
         adapter = MainAdapter(requireActivity().applicationContext)
         recycler.layoutManager = GridLayoutManager(requireActivity().applicationContext,1)
@@ -66,8 +74,10 @@ class HomeFragment : Fragment() {
     }
 
     fun observerData(){
-           // shimmer_view_container.startShimmer()
+
+
         viewModel.fetchLocalData().observe(viewLifecycleOwner, Observer {
+            //swipeRefreshLayout.isRefreshing=false
             shimmer_view_container.stopShimmer()
             shimmer_view_container.visibility=View.GONE
             adapter.setListData(it)
@@ -75,6 +85,8 @@ class HomeFragment : Fragment() {
 
         })
     }
+
+
 
 }
 
