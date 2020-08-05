c80cf0d955f2e533bb55d1e9643c87d23a4f400d
ianmajkut <ianedwinmajkut@gmail.com>
2020-08-05 15:37:51 -0300

    shimmer effect en fila usuarios local

--- app/src/main/java/com/qnet/qnetclient/applocal/ui/fila/FilaLocalFragment.kt
+++ app/src/main/java/com/qnet/qnetclient/applocal/ui/fila/FilaLocalFragment.kt
@@ -10,9 +10,11 @@ import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.ShimmerFrameLayout
 import com.qnet.qnetclient.R
 import com.qnet.qnetclient.appusuario.ui.fila.AdapterFila
 import com.qnet.qnetclient.viewModel.FirestoreViewModel
+import kotlinx.android.synthetic.main.fragment_fila.*
 
 
 class FilaLocalFragment : Fragment() {
@@ -27,10 +29,14 @@ class FilaLocalFragment : Fragment() {
         val layout = inflater.inflate(R.layout.fragment_fila_local, container, false)
         num = layout.findViewById(R.id.cantidad_clientes)
         val recycler = layout.findViewById<RecyclerView>(R.id.recyclerview)
+        val shimmer_view_container=layout.findViewById<ShimmerFrameLayout>(R.id.shimmer_view_container)
 
         recycler.layoutManager = GridLayoutManager(requireActivity().applicationContext,1)
         adapter = AdapterLocal(requireActivity().applicationContext)
         recycler.adapter = adapter
+
+        shimmer_view_container.startShimmer()
+
         observerData()
 
         return layout
@@ -38,6 +44,8 @@ class FilaLocalFragment : Fragment() {
     }
     private fun observerData(){
         viewModel.fetchUsuarios().observe(viewLifecycleOwner, Observer {
+            shimmer_view_container.stopShimmer()
+            shimmer_view_container.visibility = View.GONE
             adapter.setListData(it)
             adapter.notifyDataSetChanged()
             num.text = it.size.toString()
