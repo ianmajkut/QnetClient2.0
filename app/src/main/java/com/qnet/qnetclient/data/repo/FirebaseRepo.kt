f874e73490415ea50696c9abd41f929ba7719c49
Lucas Affre <lucasaffre@gmail.com>
2020-08-05 16:34:58 -0300

    ImageChange

--- app/src/main/java/com/qnet/qnetclient/data/repo/FirebaseRepo.kt
+++ app/src/main/java/com/qnet/qnetclient/data/repo/FirebaseRepo.kt
@@ -53,7 +53,10 @@ class FirebaseRepo {
 
     fun uploadImage(uri: Uri?,info:InfoRegister):LiveData<Boolean>{
         val mutableData = MutableLiveData<Boolean>()
-        if (uri == null) return mutableData
+        if (uri == null){
+            mutableData.value = false
+            return mutableData
+        }
         val filename = UUID.randomUUID().toString()
         val ref= FirebaseStorage.getInstance().getReference("/images/$filename")
 
@@ -461,6 +464,34 @@ class FirebaseRepo {
             .addOnFailureListener {
                 mutableData.value = false
             }
+
+        return mutableData
+    }
+    fun changeImage(uri:Uri?):LiveData<Boolean>{
+        val mutableData = MutableLiveData<Boolean>()
+        if (uri == null){
+            mutableData.value = false
+            return mutableData
+        }
+        val filename = UUID.randomUUID().toString()
+        val ref= FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(uri).addOnSuccessListener {
+            ref.downloadUrl.addOnSuccessListener {
+
+            }
+        }.addOnFailureListener{
+            mutableData.value= false
+        }
+
+        return mutableData
+    }
+    private fun deleteImage(path: String?):LiveData<Boolean>{
+        val mutableData = MutableLiveData<Boolean>()
+        mAuth = FirebaseAuth.getInstance()
+        db.document("locales/${mAuth.currentUser?.uid}").get().addOnSuccessListener {
+            val image = it.getString("image")
+        }
 
         return mutableData
     }
